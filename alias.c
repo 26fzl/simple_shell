19ad473e3fa495063cd5c75c9c8b40200465bcc5
fatima zahra lamrani <fatimazahralamrani26@gmail.com>
2023-06-27 14:47:39 -0700

    0x016 simple shell

--- alias.c
+++ alias.c
@@ -13,7 +13,7 @@ int print_alias(data_of_program *data, char *alias)
 
 	if (data->alias_list)
 	{
-		data = str_length(alias);
+		alias_length = str_length(alias);
 		for (x = 0; data->alias_list[x]; x++)
 		{
 			if (!alias || (str_compare(data->alias_list[x], alias, alias_leng)
